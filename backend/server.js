bf6a321b13a11e8510166a1a27cb0b9c98640c3f
ADERLY ELIAS TORRES ARTEAGA <2101010367@undc.edu.pe>
2025-09-17 17:10:55 -0500

    erro de ccs y front

--- backend/server.js
+++ backend/server.js
@@ -46,6 +46,17 @@ app.use('/api/history', require('./routes/history.routes.js'));
 app.use('/api/config', require('./routes/config.routes'));
 app.use('/api/auth', require('./routes/auth.routes')); // Corrected path
 
+// Serve frontend static files in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  // Serve index.html for all other routes
+  app.get('*', (req, res) => 
+    res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'))
+  );
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
